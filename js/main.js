20060a0fe1ac8f94dd6043999a346274be73fb6f
atefeh97hmt <hemmati.atefeh97@gmail.com>
2020-10-18 17:04:36 +0330

    color change with js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// red circle
+document.getElementById("changeGray").onclick = function(){
+	document.getElementById("red-circle").style.background = 'gray';
+}
+
+
+
+
+//blue circle
+document.getElementById("changePink").onclick = function(){
+
+    document.getElementById("blue-circle").style.background ="deeppink";
+}
+
+
+
+//green circle
+document.getElementById("changeGold").onclick = function(){
+
+document.getElementById("green-circle").style.background = 'gold';
+
+}
